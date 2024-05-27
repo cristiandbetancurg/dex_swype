a5328d6eb6d9fef54c7613d646acfba7c2862e33
Cristian Betancur <34930340+cristiandbetancurg@users.noreply.github.com>
2024-05-26 22:41:34 -0400

    Update factory.sol
    Add almacenamiento de datos

--- factory.sol
+++ factory.sol
@@ -1,1 +1,10 @@
+// ©SHIBA SWYPE 2023. ALL RIGHTS RESERVED.
+pragma solidity ^0.8.23;
+
+import "./interfaces/IPair.sol"; 
+
+contrac Factory {
+    address public feeTo; // Dirección para recibir las tarifas
+    address public feeToSetter; // Dirección que puede cambiar feeTo
+}
 
